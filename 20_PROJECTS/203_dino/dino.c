95ce82158e124194ae0f5f8d4bc713d7b6cb48ae
JackeLeMort <megninpreiss.ivanoe@gmail.com>
2024-02-22 17:58:17 +0100

    dino +

--- 20_PROJECTS/203_dino/dino.c
+++ 20_PROJECTS/203_dino/dino.c
@@ -2,11 +2,22 @@
 
 int main(){
   initscr();
+
+  WINDOW * win;
+
+  move(10,10);
+  box(win, 15, 20);
+  getch();
+
   addstr("Hello World!");
   refresh;
+
   addstr("teeees!");
   refresh;
+
   getch();
+
   endwin();
+
   return 0;
 }
