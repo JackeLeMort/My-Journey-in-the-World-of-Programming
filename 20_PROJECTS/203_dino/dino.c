a2c0ee37f22d88eb5ed13d0e74878999aad750f7
JackeLeMort <megninpreiss.ivanoe@gmail.com>
2024-02-23 17:58:33 +0100

    started messing around with ncurses

--- 20_PROJECTS/203_dino/dino.c
+++ 20_PROJECTS/203_dino/dino.c
@@ -1,23 +1,39 @@
 #include "ncurses.h"
+#include "unistd.h"
+
+int check_q();
 
 int main(){
+
   initscr();
+  cbreak();
+  clear();
 
-  WINDOW * win;
+  int y = LINES;
+  int x = COLS;
 
-  move(10,10);
-  box(win, 15, 20);
+  for (int i = 0; i >= 0; i++){
+
+    if (LINES-i < 0 || COLS-i < 0){
+      clear();
+      mvaddstr(LINES/2, COLS/2, "went out of the screen");
+
+      check_q();
+    }
+    move( y-i , x-i);
+    addch('.');
+    getch();
+//    sleep(1);
+  }
   getch();
-
-  addstr("Hello World!");
-  refresh;
-
-  addstr("teeees!");
-  refresh;
-
-  getch();
-
   endwin();
 
   return 0;
+}
+
+int check_q(){
+  c = getch();
+  for (c != "q"){
+    c = getch();
+  }
 }
