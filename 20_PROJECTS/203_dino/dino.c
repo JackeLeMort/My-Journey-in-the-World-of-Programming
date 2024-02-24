fcd80caeeb071f6af039bd294fe83e944f58c7ca
JackeLeMort <megninpreiss.ivanoe@gmail.com>
2024-02-24 11:47:31 +0100

    start dino.c for real, creater main function

--- 20_PROJECTS/203_dino/dino.c
+++ 20_PROJECTS/203_dino/dino.c
@@ -1,39 +1,56 @@
 #include "ncurses.h"
 #include "unistd.h"
+#include "ctype.h"
+#include "stdlib.h"
 
-int check_q();
+//function
+int start_screen(int speed);
+int game(int speed);
+int end_screen(int score);
 
-int main(){
+//variables, several speed possible
+int speed_default = 2;
+int score_best = 0;
+int score_last = 0;
 
+int main(int argc, char **argv){
+
+  if (argc > 2){
+    printf("The programm needs 0 or 1 argument.");
+    return 1;
+  }
+  for (int i = 0; i < strlen(argv[1]); i++){
+    if ( isdigit(argv[1][i]) ){
+      printf("The argument must be an integer.");
+      return 1;
+  }
+  if (argc == 1){
+    int speed = speed_default;
+  }
+  else if (argc == 2){
+    speed = atoi (argv[1]);
+  }
+
+  start_screen(speed);
+
+  game(speed);
+
+}
+
+int start_screen(int speed);
+
+int game(int speed){
   initscr();
   cbreak();
+  noecho();
+
   clear();
 
   int y = LINES;
   int x = COLS;
 
-  for (int i = 0; i >= 0; i++){
 
-    if (LINES-i < 0 || COLS-i < 0){
-      clear();
-      mvaddstr(LINES/2, COLS/2, "went out of the screen");
-
-      check_q();
-    }
-    move( y-i , x-i);
-    addch('.');
-    getch();
-//    sleep(1);
-  }
-  getch();
   endwin();
-
   return 0;
 }
 
-int check_q(){
-  c = getch();
-  for (c != "q"){
-    c = getch();
-  }
-}
