67ded8e109d7befb8d13caef03e1197704ed8379
e-v-o-l-v-e <megninpreiss.ivanoe@gmail.com>
2024-03-12 19:38:59 +0100

    open source and destination files

--- 30_PROJECTS_WIP/301_cp/cp.c
+++ 30_PROJECTS_WIP/301_cp/cp.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int main (int argc, char *argv)
+int main (int argc, char **argv)
 {
   if (argc < 2 || argc > 3){
-    printf ("Usage: ./cp file_to_copy (optionnal: new_filename)");
+    printf ("Usage: ./cp file_to_copy (optionnal: new_filename)\n");
   }
 
-  FILE *source = fopen(argv[1], r);
 
- # FILE *destinati
+  FILE *source = fopen (argv[1], "rb");
+  FILE *destination = fopen (argv[2], "wb");
 
+  fread (source, )
+
+  fclose(source);
+  fclose(destination);
 }
